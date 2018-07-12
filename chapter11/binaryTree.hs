d68d926f8f762259915ab5acdf3fc4f5cc9d6ff8
Abhigya Maskay <abhigya@enterpriseesupport.com>
2018-07-12 14:22:49 -0400

    Binary Tree exercise done

--- chapter11/binaryTree.hs
+++ chapter11/binaryTree.hs
@@ -24,3 +24,46 @@ mapOkay =
   if mapTree (+1) testTree' == mapExpected
   then print "Ok"
   else error "test failed!"
+
+preorder :: BinaryTree a -> [a]
+preorder Leaf = []
+preorder (Node left a right) = [a] ++ (preorder left) ++ (preorder right)
+
+inorder :: BinaryTree a -> [a]
+inorder Leaf = []
+inorder (Node left a right) = (inorder left) ++ [a] ++ (inorder right)
+
+postorder :: BinaryTree a -> [a]
+postorder Leaf = []
+postorder (Node left a right) = (postorder left) ++ (postorder right) ++ [a]
+
+testTree :: BinaryTree Integer
+testTree =
+  Node (Node Leaf 1 Leaf)
+       2
+       (Node Leaf 3 Leaf)
+
+testPreorder :: IO ()
+testPreorder =
+  if preorder testTree == [2,1,3]
+  then putStrLn "Preorder fine!"
+  else putStrLn "Preorder test failed!"
+
+testInorder :: IO ()
+testInorder =
+  if inorder testTree == [1,2,3]
+  then putStrLn "Inorder fine!"
+  else putStrLn "Inorder test failed"
+
+testPostorder :: IO ()
+testPostorder =
+  if postorder testTree == [1,3,2]
+  then putStrLn "Postorder fine!"
+  else putStrLn "Postorder test failed"
+
+foldTree :: (a -> b -> b)
+         -> b
+         -> BinaryTree a
+         -> b
+foldTree f start Leaf = start
+foldTree f start tree = foldr f start (inorder tree)
