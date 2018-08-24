e8da42f1a689c1b7d487dce377b9c12202ddfa25
Abhigya Maskay <abhigya@enterpriseesupport.com>
2018-08-24 13:24:37 -0400

    Finished Reader Monad exercise

--- chapter22/ReaderMonad.hs
+++ chapter22/ReaderMonad.hs
@@ -20,5 +20,31 @@ instance Monad (Reader r) where
   (>>=) :: Reader r a -> (a -> Reader r b) -> Reader r b
   (Reader ra) >>= aRb = join $ Reader $ \r -> aRb (ra r)
 
+getDogRM :: Person -> Dog
+getDogRM = do
+  name <- dogName
+  addy <- address
+  return $ Dog name addy
+
+getDogRM' :: Reader Person Dog
+getDogRM' = Dog <$> Reader dogName <*> Reader address
+
+newtype HumanName = HumanName String deriving (Eq, Show)
+newtype DogName = DogName String deriving (Eq, Show)
+newtype Address = Address String deriving (Eq, Show)
+
+data Person = Person {
+      humanName :: HumanName
+    , dogName :: DogName
+    , address :: Address
+    } deriving (Eq, Show)
+
+data Dog = Dog {
+      dogsName :: DogName
+    , dogsAddress :: Address
+    } deriving (Eq, Show)
+
+
+
 main :: IO()
 main = undefined
