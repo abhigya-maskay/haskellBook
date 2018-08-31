bf622d975e86e57b507eab4b0ab14be135583ef2
Abhigya Maskay <abhigya@enterpriseesupport.com>
2018-08-31 11:38:48 -0400

    Finished Unit of Success exercise

--- chapter24/parsingFractions/src/Main.hs
+++ chapter24/parsingFractions/src/Main.hs
@@ -1,5 +1,42 @@
-module Main where
+{-# LANGUAGE OverloadedStrings #-}
 
-main :: IO ()
+module Text.Fractions where
+
+import Control.Applicative
+import Data.Ratio ((%))
+import Text.Trifecta
+
+badFraction :: String
+badFraction = "1/0"
+
+alsoBad :: String
+alsoBad = "10"
+
+shouldWork :: String
+shouldWork = "1/2"
+
+shouldAlsoWork :: String
+shouldAlsoWork = "2/1"
+
+parseFraction :: Parser Rational
+parseFraction = do
+  numerator <- decimal
+  char '/'
+  denominator <- decimal
+  return (numerator % denominator)
+
+parseInts :: Parser Integer
+parseInts = do
+  int <- integer
+  _   <- eof
+  return int
+
+main :: IO()
 main = do
-  putStrLn "hello world"
+  let parseFraction' = parseString parseFraction mempty
+  print $ parseFraction' shouldWork
+  print $ parseFraction' shouldAlsoWork
+  print $ parseString (parseInts) mempty "123"
+  print $ parseString (parseInts) mempty "123abc"
+  print $ parseFraction' badFraction
+  print $ parseFraction' alsoBad
