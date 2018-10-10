4df72f1bc70eaf0cd15ede667a1861c00021caab
Abhigya Maskay <ave70011@gmail.com>
2018-10-10 16:51:08 -0400

    Finished marshalling example

---
+++ chapter24/marshalling/src/Main.hs
@@ -0,0 +1,56 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
+
+module Marshalling where
+
+  import Control.Applicative
+  import Data.Aeson
+  import Data.ByteString (ByteString)
+  import qualified Data.Text as T
+  import Data.Text (Text)
+  import Text.RawString.QQ
+
+  sectionJson :: ByteString
+  sectionJson = [r|
+  { "section": {"host": "wikipedia.org"},
+    "whatisit": {"red": "intoothandclaw"}
+  }
+  |]
+
+  data TestData = 
+    TestData {
+      section :: Host
+    , what :: Color
+    } deriving (Eq, Show)
+  
+  instance FromJSON TestData where
+    parseJSON (Object v) = 
+      TestData <$> v .: "section"
+               <*> v .: "whatisit"
+
+  newtype Host = Host String deriving (Eq, Show)
+  instance FromJSON Host where
+    parseJSON (Object v) =
+      Host <$> v .: "host"
+    parseJSON _ =
+      fail "Expected an object for Host"
+
+  type Annotation = String
+
+  data Color = Red Annotation
+              | Blue Annotation
+              | Yellow Annotation
+              deriving (Eq, Show)
+  instance FromJSON Color where
+    parseJSON (Object v) = 
+          (Red <$> v .: "red")
+      <|> (Blue <$> v .: "blue")
+      <|> (Yellow <$> v .: "yellow")
+    parseJSON _ =
+      fail "Expected an object for Color"
+
+  main :: IO()
+  main = do
+    let blah :: Maybe Value
+        blah = decodeStrict sectionJson
+    print blah
