0892a3e81c4dbbbe645423920e2b8138fb12a53d
Abhigya Maskay <ave70011@gmail.com>
2018-10-16 09:53:45 -0400

    Added show to IPAddress6 datatype

--- chapter24/ipv6/src/Main.hs
+++ chapter24/ipv6/src/Main.hs
@@ -7,14 +7,60 @@ module Main where
   import Data.Maybe
   import Text.Trifecta
   import Control.Applicative
+  import Data.List.Split
+  import Control.Monad
+  import Control.Arrow
 
-  data IPAddress6 = IPAddress6 Word64 Word64 deriving (Eq, Ord, Show)
+  data IPAddress6 = IPAddress6 Word64 Word64 deriving (Eq, Ord)
+  instance Show IPAddress6 where
+    show =  show . intercalate ":" . createIpString . hexIpList where 
+      hexIpList = splitToHex . ipToBinary
+
+  createIpString :: [String] -> [String]
+  createIpString s = go s False [] where
+    maxNum = findMax s
+    isMaxOccurence xs = (length . takeWhile (== "0") $ xs) == maxNum
+    go [] doubleColon acc = reverse acc
+    go ("0":[]) False acc = reverse (":" : acc )
+    go ("0":xs) False acc = 
+      if (isMaxOccurence ("0":xs)) then
+        go (dropWhile (== "0") (xs)) True (":" : acc) else
+          go xs False ("0" : acc)
+    go (x:xs) doubleColon acc = go xs doubleColon (x:acc)
+
+  ipToBinary :: IPAddress6 -> String
+  ipToBinary (IPAddress6 a b) = (binary a ++ binary b) where
+    binary :: Word64 -> String
+    binary w = leftPad 64 . decimalToBinary $ ((fromIntegral w) :: Integer)
+
+  splitToHex :: String -> [String]
+  splitToHex = capitalizeLetters . fmap decimalToHex . fmap binaryToDecimal <$> splitPlaces (take 8 $ repeat 16) 
+
+  findMax :: [String] -> Int
+  findMax s = go s 0 0 where
+    go ("0":[]) acc prev = max acc (prev+1)
+    go (x:[]) acc prev = max acc prev
+    go ("0":xs) acc prev = go xs acc (prev + 1)
+    go (x:xs) acc prev = go xs (max acc prev) 0
 
   leftPad :: Integer -> String -> String
   leftPad n s = (take (fromIntegral (n - (genericLength s))) $ repeat '0') ++ s
 
   decimalToBinary :: Integer -> String
   decimalToBinary n = leftPad 16 $ showIntAtBase 2 intToDigit n ""
+
+  binaryToDecimal :: String -> Integer
+  binaryToDecimal = fst . fromJust . listToMaybe . readInt 2 (`elem` "01") digitToInt
+
+  decimalToHex :: Integer -> String
+  decimalToHex n = showIntAtBase 16 intToDigit n ""
+
+  capitalizeLetters :: [String] -> [String]
+  capitalizeLetters s = fmap capitalize <$> s where
+    capitalize :: Char -> Char
+    capitalize s = if (isAlpha s) then
+      toUpper s else
+        s
 
   hexToDecimal :: String -> Integer
   hexToDecimal = fst . fromJust . listToMaybe . readHex 
@@ -50,8 +96,8 @@ module Main where
       fst p else
         fst p ++ (take (missing * 16) $ repeat '0')
 
-  parseIp :: Parser IPAddress6
-  parseIp = do
+  parseIpv6 :: Parser IPAddress6
+  parseIpv6 = do
     a <- binaryToIp . foldAndHalf . addSections . (fmap hexToBinaryTuple) . foldList <$> some parseIpSec
     return a
 
